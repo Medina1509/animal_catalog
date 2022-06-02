c2c0a36d0da5da85d981698429d66f13d7cf7e4f
medinakalmyk <kalmykme@gmail.com>
2022-06-02 19:10:47 +0300

    Add firestore dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(MyApp());
 }
+
+FirebaseFirestore firestore = FirebaseFirestore.instance;
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
